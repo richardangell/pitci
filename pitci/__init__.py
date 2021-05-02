3689724c23983ef3192113b6e7579b54dc2348a9
richardangell <richardangell37@gmail.com>
2021-05-02 17:41:58 +0100

    remove old intervals module

--- pitci/__init__.py
+++ pitci/__init__.py
@@ -1,10 +1,9 @@
 from pitci._version import __version__
-from pitci.intervals import (
-    AbsoluteErrorConformalPredictor,
-    LeafNodeScaledConformalPredictor,
+from pitci.xgboost import (
+    XGBoosterAbsoluteErrorConformalPredictor,
+    XGBoosterLeafNodeScaledConformalPredictor,
 )
 import pitci.helpers as helpers
 import pitci.checks as checks
-import pitci.intervals as intervals
 import pitci.base as base
 import pitci.xgboost as xgboost
