edae2df8961711b87b64e2ca54c663ccaa4ca048
richardangell <richardangell37@gmail.com>
2021-09-03 09:17:33 +0100

    add baseline conformal prediction values tests for the split conformal predictor classes

--- tests/lightgbm/test_LGBMBoosterLeafNodeSplitConformalPredictor.py
+++ tests/lightgbm/test_LGBMBoosterLeafNodeSplitConformalPredictor.py
@@ -21,3 +21,65 @@ def test_mro():
     assert (
         LGBMBoosterLeafNodeSplitConformalPredictor.__mro__ == expected_mro
     ), "mro not correct for LGBMBoosterLeafNodeSplitConformalPredictor"
+
+
+class TestConformalPredictionValues:
+    """Baseline tests of the conformal predictions from the
+    XGBoosterLeafNodeScaledConformalPredictor class.
+    """
+
+    def test_conformal_predictions(
+        self, lgbmbooster_diabetes_model, split_diabetes_data_into_4
+    ):
+        """Test that the conformal intervals are as expected."""
+
+        confo_model = pitci.get_leaf_node_split_conformal_predictor(
+            lgbmbooster_diabetes_model
+        )
+
+        confo_model.calibrate(
+            data=split_diabetes_data_into_4[6],
+            alpha=0.8,
+            response=split_diabetes_data_into_4[7],
+        )
+
+        assert confo_model.baseline_intervals.tolist() == [
+            149850.56265605448,
+            137606.102224906,
+            131990.08520407058,
+        ], "baseline_intervals not calculated as expected on diabetes dataset"
+
+        predictions_test = confo_model.predict_with_interval(
+            split_diabetes_data_into_4[6]
+        )
+
+        assert (
+            round(float(predictions_test[:, 1].mean()), 7) == 158.5720143
+        ), "mean test sample predicted value not calculated as expected on diabetes dataset"
+
+        expected_interval_distribution = {
+            0.0: 177.04907472041657,
+            0.05: 178.22323227337145,
+            0.1: 178.77408054789797,
+            0.2: 179.99190056266366,
+            0.3: 180.86654654279693,
+            0.4: 182.2483406993801,
+            0.5: 183.71976265007476,
+            0.6: 184.84256671689613,
+            0.7: 187.08785641320517,
+            0.8: 189.78042091525793,
+            0.9: 192.0194880043173,
+            0.95: 192.58427120717985,
+            1.0: 193.85583784741846,
+            "mean": 184.4546880645055,
+            "std": 4.882189594738741,
+            "iqr": 7.5686412781202534,
+        }
+
+        actual_interval_distribution = pitci.helpers.check_interval_width(
+            intervals_with_predictions=predictions_test
+        ).to_dict()
+
+        assert (
+            expected_interval_distribution == actual_interval_distribution
+        ), "conformal interval distribution not calculated as expected"
--- tests/xgboost/test_XGBoosterLeafNodeSplitConformalPredictor.py
+++ tests/xgboost/test_XGBoosterLeafNodeSplitConformalPredictor.py
@@ -21,3 +21,57 @@ def test_mro():
     assert (
         XGBoosterLeafNodeSplitConformalPredictor.__mro__ == expected_mro
     ), "mro not correct for XGBoosterLeafNodeSplitConformalPredictor"
+
+
+class TestConformalPredictionValues:
+    """Baseline tests of the conformal predictions from the
+    XGBoosterLeafNodeScaledConformalPredictor class.
+    """
+
+    def test_conformal_predictions(self, xgbooster_diabetes_model, diabetes_xgb_data):
+        """Test that the conformal intervals are as expected."""
+
+        confo_model = pitci.get_leaf_node_split_conformal_predictor(
+            xgbooster_diabetes_model
+        )
+
+        confo_model.calibrate(data=diabetes_xgb_data[3], alpha=0.8)
+
+        assert confo_model.baseline_intervals.tolist() == [
+            52449.67460632324,
+            35938.095474243164,
+            30433.527114868164,
+        ], "baseline_intervals not calculated as expected on diabetes dataset"
+
+        predictions_test = confo_model.predict_with_interval(diabetes_xgb_data[3])
+
+        assert (
+            round(float(predictions_test[:, 1].mean()), 7) == 145.7608841
+        ), "mean test sample predicted value not calculated as expected on diabetes dataset"
+
+        expected_interval_distribution = {
+            0.0: 154.24075310833973,
+            0.05: 161.00247370080658,
+            0.1: 165.87657841857578,
+            0.2: 174.11965960739732,
+            0.3: 180.9296751324757,
+            0.4: 186.39597083478893,
+            0.5: 189.6469418165866,
+            0.6: 196.14862368960394,
+            0.7: 206.9068895361072,
+            0.8: 211.98285764900234,
+            0.9: 225.44604332796382,
+            0.95: 231.43366627276168,
+            1.0: 315.3733379779085,
+            "mean": 195.34387263584802,
+            "std": 28.76987335271672,
+            "iqr": 32.30777296481335,
+        }
+
+        actual_interval_distribution = pitci.helpers.check_interval_width(
+            intervals_with_predictions=predictions_test
+        ).to_dict()
+
+        assert (
+            expected_interval_distribution == actual_interval_distribution
+        ), "conformal interval distribution not calculated as expected"
