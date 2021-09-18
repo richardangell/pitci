bcf411c73410963f38df45db128a62a45abd1f79
richardangell <richardangell37@gmail.com>
2021-09-18 21:27:26 +0100

    add logo into sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -68,6 +68,9 @@ autodoc_default_options = {
 #
 html_theme = "sphinx_rtd_theme"
 
+# logo to appear in the top left home page box
+html_logo = "../../logo_no-tree.png"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
